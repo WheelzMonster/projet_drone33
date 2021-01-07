d3eba7fc6fb1ab3d57f571ba20afbac3b5ec2c77
WheelzMonster <louis.escartin@hotmail.fr>
2021-01-07 10:12:17 +0100

    started GUI and added a tiny bit of doc

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ This is our internship project : drone 33. This shall be edited into further det
 
 ## xamp version **7.3.20**
 ## wordpress version **5.6**
+
+## Bibliothèques à installer si vous n'utilisez pas l'exe:
+*il faut bien évidement avoir python 3 et pip d'installé au préalable*
+
+> `pip install Pillow`
--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-print("nous sommes les meilleurs")
+from tkinter import *
 
+root = Tk()
+root.geometry("1200x800")
+root.title("sauvegarde / export drone33")
+root.resizable(False, False)
+root.config(background='black')
+
+
+# trigger functions
+def clicked_button(buttonNumber):
+    if buttonNumber == 1:
+        message = Label(root, text="J'effectue une restauration du site drone 33")
+    elif buttonNumber == 2:
+        message = Label(root, text="Je déploie un nouveau site")
+    message.grid(row=3, column=0)
+
+
+# creating a label widget and implementing elements on the grid
+button1 = Button(root, text="restauration", command=lambda: clicked_button(1)).grid(row=0, column=0)
+button2 = Button(root, text="déployer un nouveau site", command=lambda: clicked_button(2)).grid(row=1, column=0)
+
+# program loop
+root.mainloop()
