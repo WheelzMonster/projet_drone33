b4c8f163d6331e2ceeda75d182c65df400fa8926
Thibault THIEULEUX <thibault.thieuleux@epsi.fr>
2021-01-08 11:00:26 +0100

    modify the position of buttons in migration page

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ def open_migration_window():
     mig_wd.title("exportation du site")
     mig_wd.resizable(False, False)
     mig_wd.config(background='#000')
-    nom = create_button(mig_wd, "nom", 500, 500)
-    url = create_button(mig_wd, "url", 500, 500)
-    logo = create_button(mig_wd, "logo", 500, 500)
-    accueil = create_button(mig_wd, "accueil", 500, 500)
-    services = create_button(mig_wd, "services", 500, 500)
-    tele_pilotes = create_button(mig_wd, "tele_pilotes", 500, 500)
-    realisations = create_button(mig_wd, "realisations", 500, 500)
-    contact = create_button(mig_wd, "contact", 500, 500)
+    nom = create_button(mig_wd, "nom", 100, 100)
+    url = create_button(mig_wd, "url", 300, 100)
+    logo = create_button(mig_wd, "logo", 100, 200)
+    accueil = create_button(mig_wd, "accueil", 100, 300)
+    services = create_button(mig_wd, "services", 300, 300)
+    pilotes = create_button(mig_wd, "tele_pilotes", 500, 300)
+    realisations = create_button(mig_wd, "realisations", 700, 300)
+    contact = create_button(mig_wd, "contact", 900, 300)
 
 
 # all widgets in the main window
