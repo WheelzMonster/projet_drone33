eb59abd0aa99d7528812506adef301f61276f56b
WheelzMonster <louis.escartin@hotmail.fr>
2021-01-22 01:59:47 +0100

    started slider of migration window

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import tkinter.font as font
+from tkinter import filedialog
 
 root = Tk()
 root.geometry("1200x800-160-0")
@@ -20,8 +21,9 @@ def create_button(window, button_text, posx, posy):
 
 
 def create_input(window, text_label, label_posx, label_posy, input_posx, input_posy):
-    user_label = Label(window, text=text_label, font="none 14 bold", bg='#b40108', fg='#fff', bd=0, width="20", justify=CENTER).place(x=label_posx, y=label_posy)
-    user_input = Entry(window, bg='#b40108', fg='#fff', bd=0, width="60", justify=CENTER)
+    user_label = Label(window, text=text_label, font="none 14 bold", bg='#b40108', fg='#fff', bd=0, width="20",
+                       justify=CENTER).place(x=label_posx, y=label_posy)
+    user_input = Entry(window, bg='#b40108', fg='#fff', bd=0, width="35", justify=CENTER)
     font_input = font.Font(family="Segoe UI", size=13, weight='bold')
     user_input['font'] = font_input
     user_input.place(x=input_posx, y=input_posy)
@@ -33,6 +35,15 @@ def save():
     print("sauvegarde")
 
 
+def open_file(window):
+    filename = filedialog.askopenfilename(initialdir="C:/Users/louis/Desktop/dev", title="Selectionnez unlogo",
+                                          filetypes=(("png files", "*.png"), ("all files", "*.*")))
+    my_logo = ImageTk.PhotoImage(Image.open(filename))
+    image_label = Label(window, text=filename).place(x=260, y=180)
+
+
+# all widgets in the migration window
+
 def open_migration_window():
     mig_wd = Toplevel()
     mig_wd.geometry("1500x900")
@@ -40,16 +51,60 @@ def open_migration_window():
     mig_wd.title("exportation du site")
     mig_wd.resizable(False, False)
     mig_wd.config(background='#000')
-    nom = create_input(mig_wd, 'NOM', 100, 20, 400, 20)
-    url = create_input(mig_wd, 'URL', 100, 70, 400, 70)
-    # username = create_input(mig_wd, 'nom nouvel admin', 3, 0, 3, 1 )
-    # user_logo = create_button(mig_wd, "logo", 100, 150)
-    input_logo = create_input(mig_wd, "LOGO", 100, 150, 400, 150)
-    accueil = create_button(mig_wd, "accueil", 100, 300)
-    services = create_button(mig_wd, "services", 370, 300)
-    pilotes = create_button(mig_wd, "tele_pilotes", 650, 300)
-    realisations = create_button(mig_wd, "realisations", 950, 300)
-    contact = create_button(mig_wd, "contact", 1250, 300)
+    nom = create_input(mig_wd, 'nom du site', 30, 20, 300, 20)
+    url = create_input(mig_wd, 'URL', 830, 20, 1100, 20)
+    username = create_input(mig_wd, 'nom nouvel admin', 30, 50, 300, 50)
+    mdp = create_input(mig_wd, 'mdp nouvel admin', 830, 50, 1100, 50)
+    import_logo = create_button(mig_wd, 'importer un logo', 35, 100)
+    import_logo["command"] = lambda: open_file(mig_wd)
+    global img1
+    global img2
+    global img3
+    global img4
+    global img5
+    global img6
+    global img7
+    global img8
+    global img9
+    global img10
+    global img11
+    global img12
+    img1 = ImageTk.PhotoImage(Image.open("img/slider/accueil.png"))
+    img2 = ImageTk.PhotoImage(Image.open("img/slider/accueil2.png"))
+    img3 = ImageTk.PhotoImage(Image.open("img/slider/section_agriculture_vignes_zones_corrigées.png"))
+    img4 = ImageTk.PhotoImage(Image.open("img/slider/section_architecture_imobilier_zones_corrigées.png"))
+    img5 = ImageTk.PhotoImage(Image.open("img/slider/section_cartographie_mesures_zones_corrigées.png"))
+    img6 = ImageTk.PhotoImage(Image.open("img/slider/section_contact_zones_corrigées.png"))
+    img7 = ImageTk.PhotoImage(Image.open("img/slider/section_footer_zones_corrigées.png"))
+    img8 = ImageTk.PhotoImage(Image.open("img/slider/section_marketing_communication_zones_corrigées.png"))
+    img9 = ImageTk.PhotoImage(Image.open("img/slider/section_modelisation3D_zones_corrigées.png"))
+    img10 = ImageTk.PhotoImage(Image.open("img/slider/section_partenariat_zones_corrigées.png"))
+    img11 = ImageTk.PhotoImage(Image.open("img/slider/section_prix_legislation_service_zones_corrigées.png"))
+    img12 = ImageTk.PhotoImage(Image.open("img/slider/section_TV_cinema_zones_corrigées.png"))
+
+    img_list = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12]
+
+    global slider_label
+    slider_label = Label(mig_wd, image=img1)
+    slider_label.place(x=390, y=90)
+
+    def backward(img_number):
+        global slider_label
+
+    def forward(img_number, wd):
+        global slider_label
+
+        slider_label.place_forget()
+        slider_label = Label(wd, image=img_list[img_number-1])
+        button_forward = Button(mig_wd, text=">>", command=lambda: forward(img_number+1, mig_wd)).place(x=340, y=380)
+        button_back = Button(mig_wd, text="<<", command=lambda: backward(img_number-1)).place(x=340, y=310)
+        slider_label.place(x=390, y=90)
+
+
+    global button_back
+    global button_forward
+    button_back = Button(mig_wd, text="<<", command=backward).place(x=340, y=310)
+    button_forward = Button(mig_wd, text=">>", command=lambda: forward(2, mig_wd)).place(x=340, y=380)
 
 
 # all widgets in the main window
@@ -61,9 +116,6 @@ sauvegarde = create_button(root, "SAUVEGARDER", 165, 350)
 migration = create_button(root, " MIGRATION ", 835, 350)
 sauvegarde["command"] = save
 migration["command"] = open_migration_window
-
-# all widgets in the migration window
-
 
 # program loop
 root.mainloop()
